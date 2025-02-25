dfa932060efeb387038b04df0c4d5aca3c430adc
AgeingHippy <charles_mcdonald@hotmail.com>
2025-02-25 16:28:17 +0000

    Explicit healthcheck endpoint with id

--- src/main/java/co/codingnomads/deployment/controllers/HelloController.java
+++ src/main/java/co/codingnomads/deployment/controllers/HelloController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.MessageFormat;
 
 @Controller
 @Slf4j
@@ -23,8 +26,8 @@ public class HelloController {
 
     @GetMapping("/health")
     @ResponseBody
-    public String checkHealth() {
-        log.info("******* HealthCheck *******");
+    public String checkHealth(@RequestParam(required = false) String id) {
+        log.info(MessageFormat.format("******* HealthCheck ({0}) *******", id));
         return "ok";
     }
 }
