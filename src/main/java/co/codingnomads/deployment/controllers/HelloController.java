f4d4d16c7c88133ef13a9935d9646331f6e5040c
AgeingHippy <charles_mcdonald@hotmail.com>
2025-02-25 15:10:20 +0000

    Explicit healthcheck endpoint

--- src/main/java/co/codingnomads/deployment/controllers/HelloController.java
+++ src/main/java/co/codingnomads/deployment/controllers/HelloController.java
@@ -1,12 +1,16 @@
 package co.codingnomads.deployment.controllers;
 
 import co.codingnomads.deployment.services.HelloService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@Slf4j
 public class HelloController {
     @Autowired
     HelloService helloService;
@@ -15,5 +19,12 @@ public class HelloController {
     public String sayHello(Model model) {
         model.addAttribute("hello", helloService.getHello());
         return "hello";
+    }
+
+    @GetMapping("/health")
+    @ResponseBody
+    public String checkHealth() {
+        log.info("******* HealthCheck *******");
+        return "ok";
     }
 }
